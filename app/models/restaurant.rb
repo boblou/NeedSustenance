fded41779bd7759622ea2a1e9f5d22313766d869
boboolou <mynameisntbob@gmail.com>
2012-09-23 15:49:36 -0400

    HW2

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,23 +1,10 @@
-<<<<<<< HEAD
 class Restaurant
   include MongoMapper::Document
 
-  key :name, String
-  key :rating, Integer
-  key :distance, String
-  key :avgPrice, Integer
-  key :cuisine, String
+  key :name,	String
+  key :rating,	Integer
+  key :distance,	String
+  key :avgPrice,	Integer
+  key :cuisine,	String
 
 end
-=======
-class Restaurant
-	include MongoMapper::Document
-
-	key :name,	String
-	key :rating,	String
-	key :distance,	String
-	key :price,	String
-	key :cuisine,	String
-
-end
->>>>>>> dc726ec3d6c6ad434f2205478eb25fd188da60b1
