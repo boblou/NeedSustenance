0d4a854c568899894299614f33789cfdb501fffa
boboolou <mynameisntbob@gmail.com>
2012-09-23 15:30:21 -0400

    HW2

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,10 +1,10 @@
 class Restaurant
 	include MongoMapper::Document
 
-	key :name,		String
+	key :name,	String
 	key :rating,	String
 	key :distance,	String
-	key :price,		String
+	key :price,	String
 	key :cuisine,	String
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 NeedSustenance::Application.routes.draw do
+  resources :restaurants
+
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
